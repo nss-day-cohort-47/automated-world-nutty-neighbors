51d7d545cdd076d20524573afd2b11ae6da9de46
Traye <johnson.traye@yahoo.com>
2021-03-02 12:01:39 -0600

    merging main

---
+++ scripts/germany/germanyData.js
@@ -0,0 +1,61 @@
+const cities = [
+  {
+    name: "Berlin",
+    population: "3,645,000",
+    funFact: "Berlin has the largest train station in Europe."
+  },
+  {
+    name: "Munich",
+    population: "1,472,000",
+    funFact: "Munich is further north than any major US city"
+  },
+  {
+    name: "Frankfurt",
+    population: "753,056",
+    funFact: "It is the home of the Frankfurter or hotdogs."
+  }
+];
+
+const landmarks = [
+  {
+    name: "Cologne Cathedral",
+    location: "The construction of Cologne Cathedral lasted from 1248 to 1880 and too more than 600 years to build."
+  },
+  {
+    name: "Brandenburg Gate",
+    location: "The Brandenburg Gate is Berlin’s most famous landmark and where visitors could look behind the Iron Curtain during the Cold War."
+  },
+  {
+    name: "Eltz Castle",
+    location: "It looks straight out of a picture book and has 850 years of history."
+  }
+]
+
+const famousCitizens = [
+  {
+    name: "Adolf Hitler",
+    occupation: "Actor",
+    age: 56
+  },
+  {
+    name: "Albert Einstein",
+    occupation: "Mathematician and scientist",
+    age: 76
+  },
+  {
+    name: "Johann Wolfgang von Goethe",
+    occupation: "poet, playwright, and novelist",
+    age: 82
+  }
+]
+
+export const getLandmarkFunction = () => {
+  return lanmarks;
+}
+export const getCityFunction = () => {
+  return citys;
+}
+export const getFamousCitizens = () => {
+  return citizens;
+}
+
